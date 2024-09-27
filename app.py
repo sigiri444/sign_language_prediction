29c5f19f96e857d0eeff48f4d14cf10d144d445e
sigiri444 <92792931+sigiri444@users.noreply.github.com>
2024-09-27 10:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, ClientSettings
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},
-            {"urls": ["stun:stun1.l.google.com:19302"]},
-            {"urls": ["stun:stun2.l.google.com:19302"]},
-            {"urls": ["stun:stun3.l.google.com:19302"]},
-            {"urls": ["stun:stun4.l.google.com:19302"]}
-        ]},
+    rtc_configuration={"iceServers":get_ice_servers()},
     media_stream_constraints={
         "video": True,
         "audio": False,
@@ -20,7 +37,7 @@ class VideoProcessor(VideoProcessorBase):
         return frame
 
 st.title("WebRTC Test")
-webrtc_streamer(key="example", client_settings=WEBRTC_CLIENT_SETTINGS, video_processor_factory=VideoProcessor)
+webrtc_streamer(key="example", client_settings=WEBRTC_CLIENT_SETTINGS, video_processor_factory=VideoProcessor,async_processing=True)
 
 # import streamlit as st
 # from streamlit_webrtc import webrtc_streamer,VideoProcessorBase,ClientSettings,WebRtcMode
